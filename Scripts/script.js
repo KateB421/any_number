3e9a48970c6bc5c8d163c89284c5b1c1da5340d6
KateB421 <kmburton421@gmail.com>
2022-11-04 11:39:37 -0700

    Added Event Listeners to row of numbers
    Made each number clickable and randomly fill in one of the price slots with the number that was clicked on.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,20 @@
 const remainingNumbers=document.querySelectorAll('#remainingNumbers div');
-let priceNumbers=document.querySelectorAll('.prize div div');
+let priceNumbers=document.querySelectorAll('.prize div div:not(.static)');
 const restartButton=document.querySelector('#restart');
-function createPrices(){}
-function main(){}
+let prizes=[[],[],[]];
+let anArray=[0,1,2,3,4,5,6,7,8,9];
+for (let i of remainingNumbers){
+    i.addEventListener("click", function(event){
+        let aVar=true;
+        while(aVar){
+            let aPrice =priceNumbers[Math.floor(Math.random() * 10)];
+            if(aPrice.innerText=="_"){
+                aPrice.innerHTML=i.innerHTML
+                aVar=false;
+            }
+        }
+        
+    })
+}
+
+// console.log(priceNumbers);
