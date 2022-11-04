6835dae846ac91056d9f546a39e29fc87f9f047a
KateB421 <kmburton421@gmail.com>
2022-11-04 13:28:19 -0700

    Updated Event Listeners
    Made each number clickable once with a message if multiple attempts are made, and strike through the number after it has been clicked.

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,15 +5,20 @@ let prizes=[[],[],[]];
 let anArray=[0,1,2,3,4,5,6,7,8,9];
 for (let i of remainingNumbers){
     i.addEventListener("click", function(event){
-        let aVar=true;
-        while(aVar){
-            let aPrice =priceNumbers[Math.floor(Math.random() * 10)];
-            if(aPrice.innerText=="_"){
-                aPrice.innerHTML=i.innerHTML
-                aVar=false;
-            }
+        if(i.className=="clicked"){
+            alert("That number has already selected. Please choose a new number.")
         }
-        
+        else{
+            let aVar=true;
+            while(aVar){
+                let aPrice =priceNumbers[Math.floor(Math.random() * 10)];
+                if(aPrice.innerText=="_"){
+                    aPrice.innerHTML=i.innerHTML
+                    aVar=false;
+                }
+            }
+            i.className="clicked";
+        }
     })
 }
 
